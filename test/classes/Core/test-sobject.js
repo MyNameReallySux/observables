f1b13a6354bda6f98a4b5ff647d8327632566e4b
Chris Coppola <mynamereallysux@gmail.com>
2017-07-10 00:17:53 -0400

    Added tests

---
+++ test/classes/Core/test-sobject.js
@@ -0,0 +1,11 @@
+import assert from 'assert'
+import SObject from '../../../app/js/classes/Core/SObject'
+
+describe("SObject", ()=>{
+	describe("Creation", ()=>{
+		it('Empty constructor', (done)=>{
+			instance = new SObject()
+			done()
+		})
+	})
+})
