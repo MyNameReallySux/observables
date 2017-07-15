1fbe8542d3fd8f9f9a7b461cb1366a183eb27c3b
Chris Coppola <mynamereallysux@gmail.com>
2017-07-14 22:14:12 -0400

    Fixed test errors, removed unnecessary tests.

--- test/classes/Core/test-sobject.js
+++ test/classes/Core/test-sobject.js
@@ -20,42 +20,5 @@ describe("SObject", ()=>{
 			assert.ok(instance instanceof SObject)
 			done()
 		})
-		
-		// LifeCycle Tests
-		it("Should contain 'onCreate' method", (done)=>{
-			console.log(instance)
-			assert.ok(instance.hasOwnProperty('onCreate'))
-			done()
-		})
-		it("Should contain 'onCreate' method", (done)=>{
-			console.log(instance)
-			assert.ok(instance.hasOwnProperty('onCreate'))
-			done()
-		})
-		
-		it("Should contain 'onInit' method", (done)=>{
-			assert.ok(instance.hasOwnProperty('onInit'))
-			done()
-		})
-		it("Should contain 'beforeData' method", (done)=>{
-			assert.ok(instance.hasOwnProperty('beforeData'))
-			done()
-		})
-		it("Should contain 'afterData' method", (done)=>{
-			assert.ok(instance.hasOwnProperty('afterData'))
-			done()
-		})
-		it("Should contain 'onRender' method", (done)=>{
-			assert.ok(instance.hasOwnProperty('onRender'))
-			done()
-		})
-		it("Should contain 'onAttach' method", (done)=>{
-			assert.ok(instance.hasOwnProperty('onAttach'))
-			done()
-		})
-		it("Should contain 'onDestroy' method", (done)=>{
-			assert.ok(instance.hasOwnProperty('onDestroy'))
-			done()
-		})
 	})
 })
