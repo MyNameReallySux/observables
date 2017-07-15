b6aff1213d4c304020f32782868a11d41d5f81b6
Chris Coppola <mynamereallysux@gmail.com>
2017-07-14 22:04:20 -0400

    Updated config, updates type-utils, updated tests.

--- app/js/classes/Core/SObject.js
+++ app/js/classes/Core/SObject.js
@@ -1,214 +1,215 @@
-import Types from 'type-utils'
+const Types = require('type-utils').default
 
 class SObject {
-  constructor(...args) {
-    this.onCreate(...args)
-  }
+	constructor(...args) {
+		this.onCreate(...args)
+	}
 
-  /* #######################
-    Static Methods
-  ####################### */
+	/* #######################
+	  Static Methods
+	####################### */
 
-  static GetObjectIterator() {
-    return function* (obj) {
-      if (!Types.isEmpty(obj)) {
-        for (let key of Object.keys(obj)) {
-          yield [key, obj[key]];
-        }
-      }
-    }
-  }
+	static GetObjectIterator() {
+		return function* (obj) {
+			if (!Types.isEmpty(obj)) {
+				for (let key of Object.keys(obj)) {
+					yield [key, obj[key]];
+				}
+			}
+		}
+	}
 
-  /* #######################
-    Private Methods
-  ####################### */
+	/* #######################
+	  Private Methods
+	####################### */
 
-  _handleArgs(...args) {
-    this.options = this.defaults
-    
-    let data = {}
-    this.emit('before_data', data)
-    
-    if (!Types.isEmpty(args)) {
-      let data = this.beforeData(...args)
-      if (Types.isUndefined(data)) throw new Error(`Data passed into ${this.constructor.name}'s beforeData function must be returned at end of function.`)
+	_handleArgs(...args) {
+		this.options = this.defaults
 
-      data = this.afterData(...data)
-      if (Types.isUndefined(data)) throw new Error(`Data passed into ${this.constructor.name}'s afterData function must be returned at end of function.`)
+		let data = {}
+		this.emit('before_data', data)
 
-      this.options = data
-    }
-    
-    this.emit('after_data', data)
+		if (!Types.isEmpty(args)) {
+			let data = this.beforeData(...args)
+			if (Types.isUndefined(data)) throw new Error(`Data passed into ${this.constructor.name}'s beforeData function must be returned at end of function.`)
 
-  }
+			data = this.afterData(...data)
+			if (Types.isUndefined(data)) throw new Error(`Data passed into ${this.constructor.name}'s afterData function must be returned at end of function.`)
 
-  _initEvents() {
-    this.listeners = new Map()
-    this.lifeCycleEvents = {
-      create: 'onCreate',
-      init: 'onInit',
-      before_data: 'beforeData',
-      after_data: 'afterData',
-      update: 'onUpdate',
-      attach: 'onAttach',
-      render: 'onRender',
-      destroy: 'onDestroy'
-    }
-  }
+			this.options = data
+		}
 
-  _bindEvents() {
-    const iterator = SObject.GetObjectIterator()
+		this.emit('after_data', data)
 
-    for (let [event, methods] of iterator(this.lifeCycleEvents)) {
-      if (!Types.isArray(methods)) methods = [methods];
+	}
 
-      for (let method of methods) {
-        if (!this.hasOwnProperty(method)) this[method] = function () {}
-        this.addListener(event, () => {
-          console.log(`${this.constructor.name}: handling ${event} event.`)
+	_initEvents() {
+		this.listeners = new Map()
+		this.lifeCycleEvents = {
+			create: 'onCreate',
+			init: 'onInit',
+			before_data: 'beforeData',
+			after_data: 'afterData',
+			update: 'onUpdate',
+			attach: 'onAttach',
+			render: 'onRender',
+			destroy: 'onDestroy'
+		}
+	}
 
-          for (let mixin of iterator(this.mixins)) {
-            if (mixin.hasOwnProperty(event)) {
-              mixin[event]
-            }
-          }
+	_bindEvents() {
+		const iterator = SObject.GetObjectIterator()
 
-          this[method]()
-        })
-      }
-    }
-  }
+		for (let [event, methods] of iterator(this.lifeCycleEvents)) {
+			if (!Types.isArray(methods)) methods = [methods];
 
-  _initMixins() {
-    this.mixins = this.options.hasOwnProperty('mixins') ? this.options.mixins : {}
-  }
+			for (let method of methods) {
+				this.addListener(event, () => {
+					console.log(`${this.constructor.name}: handling ${event} event.`)
 
-  /* #######################
-    Public Methods
-    - Lifecycle
-  ####################### */
+					for (let mixin of iterator(this.mixins)) {
+						if (mixin.hasOwnProperty(event)) {
+							mixin[event]
+						}
+					}
 
-  onCreate(...args) {
-    this.setDefaults()
-    this._initEvents()
-    this._bindEvents()
-    this.emit('create')
-    this._handleArgs(...args)
-    this._initMixins()
-  }
+					// this[method]()
+				})
+			}
+		}
+	}
 
-  beforeData(...data) {
-    return data
-  }
-  afterData(...data) {
-    /* #######################
-      Local Functions
-    ####################### */
+	_initMixins() {
+		this.mixins = this.options.hasOwnProperty('mixins') ? this.options.mixins : {}
+	}
 
-    function handleOptionsObject(options) {
-      this.options = Object.assign({}, this.defaults, options)
-    }
+	/* #######################
+	  Public Methods
+	  - Lifecycle
+	####################### */
 
-    function handleDefaultCase() {
-      this.options = this.defaults
-    }
+	onCreate(...args) {
+		this.setDefaults()
+		this._initEvents()
+		this._bindEvents()
+		this.emit('create')
+		this._handleArgs(...args)
+		this._initMixins()
+	}
 
-    function handleSingleArg(arg) {
-      switch (Types.asString(arg)) {
-        case 'object':
-          {
-            handleOptionsObject.apply(this, arg)
-          }
-          break
-        default:
-          {
-            throw new Error(`Single argument passed into '${this.constructor.name}', is not a valid type (object). Found ${Types.asString(arg)}.`)
-          }
-      }
-    }
+	beforeData(...data) {
+		return data
+	}
+	afterData(...data) {
+		/* #######################
+		  Local Functions
+		####################### */
 
-    /* #######################
-      Procedure
-    ####################### */
+		function handleOptionsObject(options) {
+			this.options = Object.assign({}, this.defaults, options)
+		}
 
-    switch (data.length) {
-      case 1:
-        handleSingleArg.apply(this, [data[0]])
-        break
-      default:
-        handleDefaultCase.apply(this, ...data)
-    }
+		function handleDefaultCase() {
+			this.options = this.defaults
+		}
 
-    return data
-  }
+		function handleSingleArg(arg) {
+			switch (Types.asString(arg)) {
+				case 'object':
+					{
+						handleOptionsObject.apply(this, arg)
+					}
+					break
+				default:
+					{
+						throw new Error(`Single argument passed into '${this.constructor.name}', is not a valid type (object). Found ${Types.asString(arg)}.`)
+					}
+			}
+		}
 
-  setDefaults() {
-    this.defaults = {}
-  }
+		/* #######################
+		  Procedure
+		####################### */
 
-  /* #######################
-    Public Methods
-    - Event Emitter
-  ####################### */
+		switch (data.length) {
+			case 1:
+				handleSingleArg.apply(this, [data[0]])
+				break
+			default:
+				handleDefaultCase.apply(this, ...data)
+		}
 
-  addListener(label, callback) {
-    this.listeners.has(label) || this.listeners.set(label, [])
-    this.listeners.get(label).push(callback)
-  }
-  removeListener(label, callback) {
-    let listeners = this.listeners.get(label),
-      index = 0
+		return data
+	}
 
-    if (listeners && listeners.length) {
-      index = listeners.reduce((i, listener, index) => {
-        return Types.isFunction(listener) && listener === callback ? i = index : i
-      }, -1)
+	setDefaults() {
+		this.defaults = {}
+	}
 
-      if (index > -1) {
-        listeners.splice(index, 1)
-        this.listeners.set(label, listeners)
-        return true
-      }
-    }
-    return false
-  }
-  emit(label, ...args) {
-    let listeners = this.listeners.get(label)
-    if (listeners && listeners.length) {
-      listeners.forEach((listener) => {
-        Types.isFunction(listener) ? listener.apply(this.context, ...args) : undefined
-      })
-      return true
-    }
-    return false
-  }
+	/* #######################
+	  Public Methods
+	  - Event Emitter
+	####################### */
 
-  /* #######################
-    Public Methods
-    - Mixin Handler
-  ####################### */
+	addListener(label, callback) {
+		this.listeners.has(label) || this.listeners.set(label, [])
+		this.listeners.get(label).push(callback)
+	}
+	removeListener(label, callback) {
+		let listeners = this.listeners.get(label),
+			index = 0
 
-  addMixin(mixin) {
-    /* Add new mixin to mixins*/
-    this.mixins[mixin.name] = mixin
-  }
-  removeMixin(mixin) {
-    delete this.mixins[mixin.name]
-  }
-  addAllMixins(mixins) {
-    /* Append new mixins to defaults */
-    this.mixins = Object.assign({}, mixins, this.mixins)
-  }
-  createMixins() {
-    const iterator = SObject.GetObjectIterator()
-    for (let [key, mixin] of iterator(this.mixins)) {
-      let instance = mixin(this)
-      instance.onCreate(this)
-      console.log(key)
-      this.mixins[instance.name] = instance
-    }
-  }
+		if (listeners && listeners.length) {
+			index = listeners.reduce((i, listener, index) => {
+				return Types.isFunction(listener) && listener === callback ? i = index : i
+			}, -1)
+
+			if (index > -1) {
+				listeners.splice(index, 1)
+				this.listeners.set(label, listeners)
+				return true
+			}
+		}
+		return false
+	}
+	emit(label, ...args) {
+
+
+		let listeners = this.listeners.get(label)
+		if (listeners && listeners.length) {
+			listeners.forEach((listener) => {
+				Types.isFunction(listener) ? listener.apply(this.context, ...args) : undefined
+			})
+			return true
+		}
+		return false
+	}
+
+	/* #######################
+	  Public Methods
+	  - Mixin Handler
+	####################### */
+
+	addMixin(mixin) {
+		/* Add new mixin to mixins*/
+		this.mixins[mixin.name] = mixin
+	}
+	removeMixin(mixin) {
+		delete this.mixins[mixin.name]
+	}
+	addAllMixins(mixins) {
+		/* Append new mixins to defaults */
+		this.mixins = Object.assign({}, mixins, this.mixins)
+	}
+	createMixins() {
+		const iterator = SObject.GetObjectIterator()
+		for (let [key, mixin] of iterator(this.mixins)) {
+			let instance = mixin(this)
+			instance.onCreate(this)
+			console.log(key)
+			this.mixins[instance.name] = instance
+		}
+	}
 }
 
-export default SObject
+module.exports = SObject
--- test/classes/Core/test-sobject.js
+++ test/classes/Core/test-sobject.js
@@ -1,10 +1,60 @@
-import assert from 'assert'
-import SObject from '../../../app/js/classes/Core/SObject'
+const assert = require('assert')
+const Types = require('type-utils').default
+const SObject = require('../../../app/js/classes/Core/SObject')
+
+const methodExists = function(instance, method){
+	return Object.getOwnPropertyNames(Object.getPrototypeOf(instance)).indexOf(method) > -1
+}
 
 describe("SObject", ()=>{
-	describe("Creation", ()=>{
-		it('Empty constructor', (done)=>{
+	describe("#Constructor (no args)", function(){
+		let instance
+		
+		before(function(){
 			instance = new SObject()
+			
+			methodExists(instance, '_handleArgs')
+		})
+		
+		it("Should be instance of 'SObject'", (done)=>{
+			assert.ok(instance instanceof SObject)
+			done()
+		})
+		
+		// LifeCycle Tests
+		it("Should contain 'onCreate' method", (done)=>{
+			console.log(instance)
+			assert.ok(instance.hasOwnProperty('onCreate'))
+			done()
+		})
+		it("Should contain 'onCreate' method", (done)=>{
+			console.log(instance)
+			assert.ok(instance.hasOwnProperty('onCreate'))
+			done()
+		})
+		
+		it("Should contain 'onInit' method", (done)=>{
+			assert.ok(instance.hasOwnProperty('onInit'))
+			done()
+		})
+		it("Should contain 'beforeData' method", (done)=>{
+			assert.ok(instance.hasOwnProperty('beforeData'))
+			done()
+		})
+		it("Should contain 'afterData' method", (done)=>{
+			assert.ok(instance.hasOwnProperty('afterData'))
+			done()
+		})
+		it("Should contain 'onRender' method", (done)=>{
+			assert.ok(instance.hasOwnProperty('onRender'))
+			done()
+		})
+		it("Should contain 'onAttach' method", (done)=>{
+			assert.ok(instance.hasOwnProperty('onAttach'))
+			done()
+		})
+		it("Should contain 'onDestroy' method", (done)=>{
+			assert.ok(instance.hasOwnProperty('onDestroy'))
 			done()
 		})
 	})
